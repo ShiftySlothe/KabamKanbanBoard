71fe17826fb9f6a0b5a9da43465c2bc2a428fd3b
ShiftySlothe <forson.charlesj@gmail.com>
2021-09-01 16:08:28 +0100

    Finished GOAuth w/ Heroku deployment

--- services/passport.js
+++ services/passport.js
@@ -23,6 +23,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     // Set up logic for when strategy is triggered
     (accessToken, refreshToken, profile, done) => {
