978a29b97c4bf488f7bda7f9cf835f0163300af1
ShiftySlothe <forson.charlesj@gmail.com>
2021-09-01 15:35:44 +0100

    Debugging prod OAuth errors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
 
+console.log("Google client ID: " + keys.googleClientID);
 //Run scripts
 require("./models/user");
 require("./services/passport");
